c448cbae091cbd4da36b51b917d4dda8e32d49b6
FZManrique <123621148+FZManrique@users.noreply.github.com>
2024-10-27 12:26:08 +0800

    Initial terrain

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,12 +10,19 @@ func _physics_process(delta: float):
 	var direction: Vector2
 	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+
+	# Sprinting          
+	if Input.is_action_pressed("ui_sprint"):
+		speed = 100
+	elif Input.is_action_just_released("ui_sprint"):
+		speed = 50  
+
 	# If input is digital, normalize it for diagonal movement
 	if abs(direction.x) == 1 and abs(direction.y) == 1:
 		direction = direction.normalized()
-	
+
 	var movement = speed * direction * delta
+	
 	move_and_collide(movement)
 	player_animations(direction)
 
@@ -28,22 +35,25 @@ func player_animations(direction: Vector2):
 		animation  = "idle_" + returned_direction(new_direction)
 		animation_sprite.play(animation)
 
-func returned_direction(direction : Vector2):
+func returned_direction(direction: Vector2) -> StringName:
 	var normalized_direction  = direction.normalized()
-	var default_return = "side"
 
 	if normalized_direction.y > 0:
 		return "down"
 	elif normalized_direction.y < 0:
 		return "up"
 	elif normalized_direction.x > 0:
-		#(right)
+		# Right direction
 		$AnimatedSprite2D.flip_h = false
 		return "side"
 	elif normalized_direction.x < 0:
-		#flip the animation for reusability (left)
+		# Left direction; we flip the image for reusability
 		$AnimatedSprite2D.flip_h = true
 		return "side"
 
-	#default value is empty
-	return default_return
+	# Default to side
+	return "side"
+	
+	# midway in https://dev.to/christinec_dev/lets-learn-godot-4-by-making-an-rpg-part-1-project-overview-setup-bgc
+	# https://docs.google.com/document/d/1486cjDI0bCL2c9y1RodzRv4qfyuKHxLhHe7jht0C744/edit?tab=t.0
+	# todo: download assets
