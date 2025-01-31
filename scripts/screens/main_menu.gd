349ec4326d8de5b35876712be2866407829c30d9
FZManrique <123621148+FZManrique@users.noreply.github.com>
2025-01-31 22:32:39 +0800

    Add background, tweak settings

--- scripts/screens/main_menu.gd
+++ scripts/screens/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var home := $Home as VBoxContainer
-@onready var settings := $Settings as VBoxContainer
+@onready var settings := $Settings as PanelContainer
 @onready var help := $Help as VBoxContainer
 
 func _ready() -> void:
@@ -31,9 +31,20 @@ func _on_quit_pressed() -> void:
 	get_tree().quit() 
 
 func _on_master_slider_changed(value: float) -> void:
-	var music_index := AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(music_index, linear_to_db(value / 100))
+	var master_index := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(master_index, linear_to_db(value / 100))
 
 func _on_music_slider_changed(value: float) -> void:
 	var music_index := AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_volume_db(music_index, linear_to_db(value / 100))
+
+func _on_sfx_slider_changed(value: float) -> void:
+	var sfx_index := AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_volume_db(sfx_index, linear_to_db(value / 100))
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if (toggled_on):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
