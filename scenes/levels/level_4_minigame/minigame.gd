c9fdf057126da1943c36bdf2f750d0282b8e498b
FZManrique <123621148+FZManrique@users.noreply.github.com>
2025-03-26 19:20:42 +0800

    Fix minigame

--- scenes/levels/level_4_minigame/minigame.gd
+++ scenes/levels/level_4_minigame/minigame.gd
@@ -3,8 +3,21 @@ extends CanvasLayer
 
 signal on_dismiss
 
+var current_pieces = 0
+
 func _ready() -> void:
 	get_tree().paused = true
+	%Exit.hide()
+	
+	for piece: PuzzlePiece in %Pieces.get_children():
+		piece.on_correct_piece_dropped.connect(
+			func():
+				current_pieces += 1
+				
+				if (current_pieces == 5):
+					%Exit.show()
+					%Pieces.process_mode = Node.PROCESS_MODE_DISABLED
+		)
 
 func _on_button_pressed() -> void:
 	get_tree().paused = false
--- scenes/levels/level_4_minigame/sprite.gd
+++ scenes/levels/level_4_minigame/sprite.gd
@@ -1,21 +1,42 @@
+class_name PuzzlePiece
 extends TextureRect
 
+@export var index: int = 1
+@export var is_final_location: bool = false
+
+var is_current_item := false
+
+signal on_correct_piece_dropped
+
+func _notification(what: int) -> void:
+	if (what == NOTIFICATION_DRAG_END):
+		if self.is_drag_successful() && is_current_item:
+			self.visible = false
+			is_current_item = false
+		elif is_current_item:
+			self.modulate.a = 1
+			is_current_item = false
+
 func _get_drag_data(at_position: Vector2) -> Variant:
+	if (is_final_location): return
 	var node = Control.new()
 	
-	var texture_rect = TextureRect.new()
+	var texture_rect := TextureRect.new()
 	texture_rect.texture = texture
 	texture_rect.rotation_degrees = 90
-	texture_rect.position = -0.5 * texture_rect.size
+	texture_rect.position = -0.3 * texture_rect.size
 	
 	node.add_child(texture_rect)
 	set_drag_preview(node)
 	
-	return texture
+	modulate.a = 0.5
+	is_current_item = true
+	
+	return duplicate()
 	
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	return data is Texture2D
+	return data is PuzzlePiece && data.index == index && !is_current_item
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
-	texture = data
-	
+	texture = (data as PuzzlePiece).texture
+	on_correct_piece_dropped.emit()
